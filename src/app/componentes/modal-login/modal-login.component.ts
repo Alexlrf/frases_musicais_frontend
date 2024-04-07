9c1761383808763e4972d53636225c30bcab0d79
AlexFogazzi <alex.fogazzi.dev@gmail.com>
2024-04-07 20:52:07 -0300

    refactor: altera nome do componente

--- src/app/componentes/modal-login/modal-login.component.ts
+++ src/app/componentes/modal-login/modal-login.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-menu-lateral',
-  templateUrl: './menu-lateral.component.html',
-  styleUrls: ['./menu-lateral.component.css']
+  selector: 'app-modal-login',
+  templateUrl: './modal-login.component.html',
+  styleUrls: ['./modal-login.component.css']
 })
-export class MenuLateralComponent implements OnInit{
+export class ModalLoginComponent implements OnInit{
 
   rotaAdmin: boolean = false
 
